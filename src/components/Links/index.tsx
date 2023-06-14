19c5da10a2d331187354b6df675f27fd00926418
Bartosz Brodowski <bartolini10058@gmail.com>
2023-06-14 21:34:04 +0200

    Add link icon and text

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,22 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Links = () => {
 	return (
-		<div className='flex gap-8'>
+		<div className='flex items-center gap-8 mt-4'>
 			<a href='https://github.com/BartoszBrodowski' target='_blank'>
-				<FontAwesomeIcon
-					className='hover:cursor-pointer text-main-black duration-300 hover:scale-125'
-					size='3x'
-					icon={faGithub}
-				/>
+				<div className='flex items-center gap-2 text-xl hover:cursor-pointer hover:text-linkedin-blue duration-300 hover:scale-110'>
+					<img className='w-10' src='src/assets/Github.svg' alt='Github logo' />
+					<p className='flex items-center gap-2'>
+						Github <FontAwesomeIcon icon={faUpRightFromSquare} />
+					</p>
+				</div>
 			</a>
 			<a href='https://www.linkedin.com/in/bartoszbrodowski/' target='_blank'>
-				<FontAwesomeIcon
-					className='hover:cursor-pointer text-linkedin-blue duration-300 hover:scale-125'
-					size='3x'
-					icon={faLinkedin}
-				/>
+				<div className='flex items-center gap-2 text-xl hover:cursor-pointer hover:text-linkedin-blue duration-300 hover:scale-110'>
+					<img className='w-10' src='src/assets/LinkedIn.svg' alt='LinkedIn logo' />
+					<p className='flex items-center gap-2'>
+						LinkedIn <FontAwesomeIcon icon={faUpRightFromSquare} />
+					</p>
+				</div>
 			</a>
 		</div>
 	);
