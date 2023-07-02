680f9c8396da7ad04ace6481b69c16d35320fa0e
BartoszBrodowski <bartolini10058@gmail.com>
2023-07-02 20:22:57 +0200

    Add responsiveness to Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   const navigate = useNavigate();
   const path = location.pathname;
   return (
-    <div className="bg-main-gray w-2/3 mx-auto absolute top-[10%] max-w-[1280px] row-span-1 col-span-5 px-3 py-2 rounded-full">
-      <ul className="flex justify-between">
+    <div className="bg-main-gray w-4/5 md:w-2/3 mx-auto absolute top-[10%] max-w-[1280px] row-span-1 col-span-5 px-3 py-2 rounded-full">
+      <ul className="flex gap-4 justify-between">
         <li
           className={`${
             path === "/" ? "bg-main-offwhite" : "bg-transparent"
-          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 px-4 duration-150 font-medium`}
+          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 md:px-4 duration-150 font-medium text-center flex-1 md:flex-none`}
           onClick={() => navigate("/")}
         >
           Home
@@ -18,30 +18,23 @@ const Navbar = () => {
         <li
           className={`${
             path === "/about" ? "bg-main-offwhite" : "bg-transparent"
-          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 px-4 duration-150 font-medium`}
+          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 md:px-4 duration-150 font-medium text-center flex-1 md:flex-none`}
           onClick={() => navigate("/about")}
         >
-          About me
+          About
         </li>
         <li
           className={`${
             path === "/projects" ? "bg-main-offwhite" : "bg-transparent"
-          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 px-4 duration-150 font-medium`}
+          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 md:px-4 duration-150 font-medium text-center flex-1 md:flex-none`}
           onClick={() => navigate("/projects")}
         >
           Projects
         </li>
-        {/* <li
-					className={`${
-						path === '/techstack' ? 'bg-main-offwhite' : 'bg-transparent'
-					} + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 px-4 duration-150 font-medium`}
-					onClick={() => navigate('/techstack')}>
-					Tech Stack
-				</li> */}
         <li
           className={`${
             path === "/contact" ? "bg-main-offwhite" : "bg-transparent"
-          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 px-4 duration-150 font-medium`}
+          } + hover:cursor-pointer hover:text-[#51545a] hover:bg-[#eaeaea] rounded-full p-1 md:px-4 duration-150 font-medium text-center flex-1 md:flex-none`}
           onClick={() => navigate("/contact")}
         >
           Contact
