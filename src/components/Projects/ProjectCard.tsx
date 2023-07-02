3c8da5635e66ecc6328f42f4a7fb0f84e76c77e0
BartoszBrodowski <bartolini10058@gmail.com>
2023-07-03 00:31:04 +0200

    Add responsiveness to Projects section

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,91 +1,114 @@
-import { motion } from 'framer-motion';
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import clsx from 'clsx';
+import { motion } from "framer-motion";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
 
 type ProjectCardProps = {
-	title: string;
-	description: string;
-	link: string;
-	isClicked: boolean;
-	setClickedId: (id: number) => void;
-	id: number;
-	clickedId: number;
-	techStack: string[];
-	animationDurationTime: number;
+  title: string;
+  description: string;
+  link: string;
+  isClicked: boolean;
+  setClickedId: (id: number) => void;
+  id: number;
+  clickedId: number;
+  techStack: string[];
+  animationDurationTime: number;
 };
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
-	title,
-	description,
-	link,
-	isClicked,
-	setClickedId,
-	id,
-	clickedId,
-	techStack,
-	animationDurationTime,
+  title,
+  description,
+  link,
+  isClicked,
+  setClickedId,
+  id,
+  clickedId,
+  techStack,
+  animationDurationTime,
 }) => {
-	const handleClick = () => {
-		if (isClicked && clickedId === id) {
-			setClickedId(-1);
-		}
-		if (clickedId === -1) {
-			setClickedId(id);
-		}
-	};
-	return (
-		<motion.div onClick={!isClicked ? handleClick : undefined}>
-			<motion.div
-				transition={{
-					layout: {
-						duration: animationDurationTime,
-						type: 'spring',
-					},
-				}}
-				layout
-				className={clsx('max-w-[530px] bg-white shadow-card rounded-lg', {
-					'shadow-full-page': isClicked && clickedId === id,
-				})}>
-				<motion.div className='cursor-pointer' onClick={() => handleClick()} layout='position'>
-					<motion.div
-						layout='position'
-						className='flex justify-between items-center cursor-pointer p-4 pb-2'>
-						<motion.h2 className='font-bold text-xl text-main-green'>{title}</motion.h2>
-						<motion.button animate={clickedId === id ? { rotate: -180 } : { rotate: 0 }} layout>
-							<FontAwesomeIcon icon={faAngleUp} />
-						</motion.button>
-					</motion.div>
-					<motion.div className='flex items-center gap-2 p-4 pt-0' layout='position'>
-						{Object.entries(techStack).map(([key, value]) => (
-							<motion.img
-								key={key}
-								className='w-6'
-								layout='position'
-								src={`${value}`}
-								alt={`${key} logo`}
-							/>
-						))}
-					</motion.div>
-				</motion.div>
-				{isClicked && (
-					<motion.div
-						className='flex flex-col gap-4 mt-4 px-4 pb-4'
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ duration: animationDurationTime }}>
-						<motion.p>{description}</motion.p>
-						<motion.div className='flex items-center gap-2'>
-							<img className='w-6' src='src/assets/images/Github.svg' alt='Github logo' />
-							<motion.a className='text-linkedin-blue hover:underline' href={link} target='_blank'>
-								{link}
-							</motion.a>
-						</motion.div>
-					</motion.div>
-				)}
-			</motion.div>
-		</motion.div>
-	);
+  const handleClick = () => {
+    if (isClicked && clickedId === id) {
+      setClickedId(-1);
+    }
+    if (clickedId === -1) {
+      setClickedId(id);
+    }
+  };
+  return (
+    <motion.div onClick={!isClicked ? handleClick : undefined}>
+      <motion.div
+        transition={{
+          layout: {
+            duration: animationDurationTime,
+            type: "spring",
+          },
+        }}
+        layout
+        className={clsx("max-w-[530px] bg-white shadow-card rounded-lg", {
+          "lg:shadow-full-page": isClicked && clickedId === id,
+        })}
+      >
+        <motion.div
+          className="cursor-pointer"
+          onClick={() => handleClick()}
+          layout="position"
+        >
+          <motion.div
+            layout="position"
+            className="flex justify-between items-center cursor-pointer p-4 pb-2"
+          >
+            <motion.h2 className="font-bold text-xl text-main-green">
+              {title}
+            </motion.h2>
+            <motion.button
+              animate={clickedId === id ? { rotate: -180 } : { rotate: 0 }}
+              layout
+            >
+              <FontAwesomeIcon icon={faAngleUp} />
+            </motion.button>
+          </motion.div>
+          <motion.div
+            className="flex items-center gap-2 p-4 pt-0"
+            layout="position"
+          >
+            {Object.entries(techStack).map(([key, value]) => (
+              <motion.img
+                key={key}
+                className="w-6"
+                layout="position"
+                src={`${value}`}
+                alt={`${key} logo`}
+              />
+            ))}
+          </motion.div>
+        </motion.div>
+        {isClicked && (
+          <motion.div
+            className="flex flex-col gap-4 mt-4 px-4 pb-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: animationDurationTime }}
+          >
+            <motion.p>{description}</motion.p>
+            <motion.div className="flex items-center gap-2">
+              <img
+                className="w-6"
+                src="src/assets/images/Github.svg"
+                alt="Github logo"
+              />
+              <motion.a
+                className="text-linkedin-blue hover:underline"
+                href={link}
+                target="_blank"
+              >
+                {link}
+              </motion.a>
+            </motion.div>
+          </motion.div>
+        )}
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default ProjectCard;
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,96 +1,102 @@
-import { useState } from 'react';
-import ProjectCard from './ProjectCard';
-import GalacticRebornCard from './GalacticRebornCard';
-import clsx from 'clsx';
-import '../../index.css';
+import { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import GalacticRebornCard from "./GalacticRebornCard";
+import clsx from "clsx";
+import "../../index.css";
 
 const durationTime = 0.6;
 
 const Projects = () => {
-	const [clickedId, setClickedId] = useState(-1);
-	const techStackImages = {
-		neo4j: 'src/assets/images/Neo4j.svg',
-		javascript: 'src/assets/images/JavaScript.svg',
-		express: 'src/assets/images/ExpressJS.svg',
-		java: 'src/assets/images/Java.svg',
-		jwt: 'src/assets/images/JWT.svg',
-		typescript: 'src/assets/images/TypeScript.svg',
-		nextjs: 'src/assets/images/NextJS.svg',
-		tailwindcss: 'src/assets/images/TailwindCSS.svg',
-	};
-	const projects = [
-		{
-			title: 'Neo4j MovieDB API',
-			description:
-				'MovieDB API project created with ExpressJS and JavaScript. It utilizes JWT to authenticate whether the user is logged in and able to access the data. All the data was stored inside Neo4j graph database.',
-			techstack: [
-				techStackImages.javascript,
-				techStackImages.express,
-				techStackImages.jwt,
-				techStackImages.neo4j,
-			],
-			link: 'https://github.com/BartoszBrodowski/neo4j-moviedb-api',
-		},
-		{
-			title: 'Galactic Reborn',
-			description:
-				'Landing page of a blockchain game project. It was created with NextJS and TailwindCSS. The site contains information about documentation, roadmap of the project, team section and community links.',
-			techstack: [techStackImages.typescript, techStackImages.nextjs, techStackImages.tailwindcss],
-			link: 'https://galacticreborn.io/',
-		},
-		{
-			title: 'Java projects',
-			description:
-				"Repository of Java projects from university classes. They allowed me to get a solid understanding of topics such as encapsulation, abstract classed, inheritance and rules of proper OOP. After the course I feel very confident in my ability to write clean Java code. (There's not many commits as the work was mostly done at the university and the time was limited).",
-			techstack: [techStackImages.java],
-			link: 'https://github.com/BartoszBrodowski/java-uni.git',
-		},
-	];
-	return (
-		<div className='grid grid-cols-3 justify-center gap-8'>
-			{projects.map((project, index) =>
-				project.title === 'Galactic Reborn' ? (
-					<div
-						className={clsx(`min-w-[300px] m-auto grid-item-${index + 1}`, {
-							'z-10 absolute w-full h-full flex justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2':
-								clickedId === index,
-						})}
-						key={index}>
-						<GalacticRebornCard
-							title={project.title}
-							description={project.description}
-							link={project.link}
-							id={index}
-							isClicked={clickedId === index}
-							setClickedId={setClickedId}
-							clickedId={clickedId}
-							techStack={project.techstack}
-							animationDurationTime={durationTime}
-						/>
-					</div>
-				) : (
-					<div
-						className={clsx(`min-w-[300px] m-auto grid-item-${index + 1}`, {
-							'absolute w-full h-full flex justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2':
-								clickedId === index,
-						})}
-						key={index}>
-						<ProjectCard
-							title={project.title}
-							description={project.description}
-							link={project.link}
-							id={index}
-							isClicked={clickedId === index}
-							setClickedId={setClickedId}
-							clickedId={clickedId}
-							techStack={project.techstack}
-							animationDurationTime={durationTime}
-						/>
-					</div>
-				)
-			)}
-		</div>
-	);
+  const [clickedId, setClickedId] = useState(-1);
+  const techStackImages = {
+    neo4j: "src/assets/images/Neo4j.svg",
+    javascript: "src/assets/images/JavaScript.svg",
+    express: "src/assets/images/ExpressJS.svg",
+    java: "src/assets/images/Java.svg",
+    jwt: "src/assets/images/JWT.svg",
+    typescript: "src/assets/images/TypeScript.svg",
+    nextjs: "src/assets/images/NextJS.svg",
+    tailwindcss: "src/assets/images/TailwindCSS.svg",
+  };
+  const projects = [
+    {
+      title: "Neo4j MovieDB API",
+      description:
+        "MovieDB API project created with ExpressJS and JavaScript. It utilizes JWT to authenticate whether the user is logged in and able to access the data. All the data was stored inside Neo4j graph database.",
+      techstack: [
+        techStackImages.javascript,
+        techStackImages.express,
+        techStackImages.jwt,
+        techStackImages.neo4j,
+      ],
+      link: "https://github.com/BartoszBrodowski/neo4j-moviedb-api",
+    },
+    {
+      title: "Galactic Reborn",
+      description:
+        "Landing page of a blockchain game project. It was created with NextJS and TailwindCSS. The site contains information about documentation, roadmap of the project, team section and community links.",
+      techstack: [
+        techStackImages.typescript,
+        techStackImages.nextjs,
+        techStackImages.tailwindcss,
+      ],
+      link: "https://galacticreborn.io/",
+    },
+    {
+      title: "Java projects",
+      description:
+        "Repository of Java projects from university classes. They allowed me to get a solid understanding of topics such as encapsulation, abstract classed, inheritance and rules of proper OOP. After the course I feel very confident in my ability to write clean Java code. (There's not many commits as the work was mostly done at the university and the time was limited).",
+      techstack: [techStackImages.java],
+      link: "https://github.com/BartoszBrodowski/java-uni.git",
+    },
+  ];
+  return (
+    <div className="lg:grid lg:grid-cols-3 flex flex-col justify-center items-start gap-8">
+      {projects.map((project, index) =>
+        project.title === "Galactic Reborn" ? (
+          <div
+            className={clsx(`min-w-[300px] flex-grow grid-item-${index + 1}`, {
+              "z-10 flex justify-center items-center absolute w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2":
+                clickedId === index,
+            })}
+            key={index}
+          >
+            <GalacticRebornCard
+              title={project.title}
+              description={project.description}
+              link={project.link}
+              id={index}
+              isClicked={clickedId === index}
+              setClickedId={setClickedId}
+              clickedId={clickedId}
+              techStack={project.techstack}
+              animationDurationTime={durationTime}
+            />
+          </div>
+        ) : (
+          <div
+            className={clsx(`min-w-[300px] flex-grow grid-item-${index + 1}`, {
+              "z-20 absolute w-full h-full flex justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2":
+                clickedId === index,
+            })}
+            key={index}
+          >
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              link={project.link}
+              id={index}
+              isClicked={clickedId === index}
+              setClickedId={setClickedId}
+              clickedId={clickedId}
+              techStack={project.techstack}
+              animationDurationTime={durationTime}
+            />
+          </div>
+        )
+      )}
+    </div>
+  );
 };
 
 export default Projects;
