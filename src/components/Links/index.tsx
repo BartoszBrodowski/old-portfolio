19e08fcdda9dee7ae7d2849d1c7c9b619654b06e
BartoszBrodowski <bartolini10058@gmail.com>
2023-07-02 22:28:12 +0200

    Fix styling for Links

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -3,7 +3,7 @@ import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Links = () => {
   return (
-    <div className="flex items-center justify-center lg:justify-start gap-3 lg:gap-8 mt-4">
+    <div className="flex items-center justify-center lg:justify-start gap-3 lg:gap-8 mt-4 lg:mt-0">
       <a href="https://github.com/BartoszBrodowski" target="_blank">
         <div className="flex items-center gap-1 text-xl hover:cursor-pointer hover:text-linkedin-blue duration-300 hover:scale-110">
           <img
@@ -11,7 +11,7 @@ const Links = () => {
             src="src/assets/images/Github.svg"
             alt="Github logo"
           />
-          <p className="flex items-center gap-1 text-sm lg:font-md">
+          <p className="flex items-center gap-1 text-sm lg:text-lg">
             Github{" "}
             <FontAwesomeIcon
               className="w-4 lg:w-5"
@@ -27,7 +27,7 @@ const Links = () => {
             src="src/assets/images/LinkedIn.svg"
             alt="LinkedIn logo"
           />
-          <p className="flex items-center gap-1 text-sm lg:font-md">
+          <p className="flex items-center gap-1 text-sm lg:text-lg">
             LinkedIn{" "}
             <FontAwesomeIcon
               className="w-4 lg:w-5"
