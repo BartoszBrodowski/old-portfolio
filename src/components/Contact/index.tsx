314509fa3f25b6cb1438ae7ecc64b90d260bd76a
Bartosz Brodowski <bartolini10058@gmail.com>
2023-07-02 17:59:48 +0200

    Add download icon to CV download button, add arrow to project cards

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
 	return (
 		<div className='flex flex-col items-center gap-12'>
 			<div className='flex flex-col items-center gap-4'>
-				<h1 className='font-bold text-6xl text-main-green'>Get in touch with me!</h1>
+				<h1 className='font-bold text-5xl text-main-green'>Get in touch with me!</h1>
 				<p className='text-xl text'>
 					If you want to hire me, offer to join a cool project, or want to ask me anything, hit me
 					up!
@@ -15,30 +15,31 @@ const Contact = () => {
 				<div>
 					<div className='flex flex-col items-left gap-8'>
 						<div className='flex gap-4 items-center'>
-							<img className='w-16' src='src/assets/images/Mail.svg' alt='Mail' />
+							<img className='w-12 color-main-green' src='src/assets/images/Mail.svg' alt='Mail' />
 							<div>
-								<p className='font-semibold text-2xl'>Mail</p>
-								<p className='text-xl text-[#808782]'>bar.brodowski@gmail.com</p>
+								<p className='font-semibold text-xl'>Mail</p>
+								<p className='text-lg text-[#808782]'>bar.brodowski@gmail.com</p>
 							</div>
 						</div>
 						<div className='flex gap-4 items-center'>
-							<img className='w-16' src='src/assets/images/Location.svg' alt='Mail' />
+							<img className='w-12' src='src/assets/images/Location.svg' alt='Mail' />
 							<div>
-								<p className='font-semibold text-2xl'>Location</p>
-								<p className='text-xl text-[#808782]'>Gdańsk, Poland</p>
+								<p className='font-semibold text-xl'>Location</p>
+								<p className='text-lg text-[#808782]'>Gdańsk, Poland</p>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div className='flex flex-col items-center gap-4'>
-					<p className='font-semibold text-xl'>Feel free to download my CV :)</p>
+					<p className='font-semibold text-lg'>Feel free to download my CV :)</p>
 					<a href={CV} download='BartoszBrodowskiCV' target='_blank' rel='noreferrer'>
 						<motion.button
 							whileHover={{ scale: 1.1 }}
 							whileTap={{ scale: 0.9 }}
 							transition={{ duration: 0.2 }}
-							className='border-4 border-main-green rounded-full px-6 py-2 font-medium text-lg'>
-							Get CV!
+							className='flex items-center gap-2 border-4 border-main-green rounded-full px-6 py-2 font-medium text-lg'>
+							<img className='w-5' src='src/assets/images/Download.svg' />
+							Get CV
 						</motion.button>
 					</a>
 				</div>
--- src/components/Projects/GalacticRebornCard.tsx
+++ src/components/Projects/GalacticRebornCard.tsx
@@ -1,103 +1,88 @@
-import { motion } from "framer-motion";
-import { faWindowMaximize, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import clsx from "clsx";
+import { motion } from 'framer-motion';
+import { faWindowMaximize, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
 
 type GalacticRebornCardProps = {
-  title: string;
-  description: string;
-  link: string;
-  isClicked: boolean;
-  setClickedId: (id: number) => void;
-  id: number;
-  clickedId: number;
-  techStack: string[];
-  animationDurationTime: number;
+	title: string;
+	description: string;
+	link: string;
+	isClicked: boolean;
+	setClickedId: (id: number) => void;
+	id: number;
+	clickedId: number;
+	techStack: string[];
+	animationDurationTime: number;
 };
 
 const GalacticRebornCard: React.FC<GalacticRebornCardProps> = ({
-  title,
-  description,
-  link,
-  isClicked,
-  setClickedId,
-  id,
-  clickedId,
-  techStack,
-  animationDurationTime,
+	title,
+	description,
+	link,
+	isClicked,
+	setClickedId,
+	id,
+	clickedId,
+	techStack,
+	animationDurationTime,
 }) => {
-  const handleClick = () => {
-    if (isClicked && clickedId === id) {
-      setClickedId(-1);
-    }
-    if (clickedId === -1) {
-      setClickedId(id);
-    }
-  };
-  return (
-    <motion.div
-      onClick={!isClicked ? handleClick : undefined}
-      transition={{
-        layout: { duration: animationDurationTime, type: "spring" },
-      }}
-      layout
-      className={clsx("max-w-[530px] bg-white shadow-card rounded-lg", {
-        "shadow-full-page": isClicked && clickedId === id,
-      })}
-    >
-      <motion.div className="cursor-pointer" onClick={() => handleClick()}>
-        <motion.div
-          layout="position"
-          className="flex justify-between items-center p-4 pb-2"
-        >
-          <motion.h2 className="font-bold text-xl text-galactic-reborn-purple">
-            {title}
-          </motion.h2>
-          <motion.button
-            animate={clickedId === id ? { opacity: 1 } : { opacity: 0 }}
-            layout="position"
-            onClick={() => handleClick()}
-          >
-            <FontAwesomeIcon icon={faXmark} />
-          </motion.button>
-        </motion.div>
-        <motion.div
-          className="flex items-center gap-2 p-4 pt-0"
-          layout="position"
-        >
-          {Object.entries(techStack).map(([key, value], index) => (
-            <motion.img
-              key={index}
-              className="w-6"
-              layout="position"
-              src={`${value}`}
-              alt={`${key} logo`}
-            />
-          ))}
-        </motion.div>
-      </motion.div>
-      {isClicked && (
-        <motion.div
-          className="flex flex-col gap-4 mt-4  px-4 pb-4"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: animationDurationTime }}
-        >
-          <motion.p>{description}</motion.p>
-          <motion.div className="flex items-center gap-2">
-            <FontAwesomeIcon icon={faWindowMaximize} />
-            <motion.a
-              className="text-linkedin-blue hover:underline"
-              href={link}
-              target="_blank"
-            >
-              {link}
-            </motion.a>
-          </motion.div>
-        </motion.div>
-      )}
-    </motion.div>
-  );
+	const handleClick = () => {
+		if (isClicked && clickedId === id) {
+			setClickedId(-1);
+		}
+		if (clickedId === -1) {
+			setClickedId(id);
+		}
+	};
+	return (
+		<motion.div
+			onClick={!isClicked ? handleClick : undefined}
+			transition={{
+				layout: { duration: animationDurationTime, type: 'spring' },
+			}}
+			layout
+			className={clsx('max-w-[530px] bg-white shadow-card rounded-lg', {
+				'shadow-full-page': isClicked && clickedId === id,
+			})}>
+			<motion.div className='cursor-pointer' onClick={() => handleClick()}>
+				<motion.div layout='position' className='flex justify-between items-center p-4 pb-2'>
+					<motion.h2 className='font-bold text-xl text-galactic-reborn-purple'>{title}</motion.h2>
+					<motion.button
+						animate={clickedId === id ? { rotate: -180 } : { rotate: 0 }}
+						layout='position'
+						onClick={() => handleClick()}>
+						<FontAwesomeIcon icon={faAngleUp} />
+					</motion.button>
+				</motion.div>
+				<motion.div className='flex items-center gap-2 p-4 pt-0' layout='position'>
+					{Object.entries(techStack).map(([key, value], index) => (
+						<motion.img
+							key={index}
+							className='w-6'
+							layout='position'
+							src={`${value}`}
+							alt={`${key} logo`}
+						/>
+					))}
+				</motion.div>
+			</motion.div>
+			{isClicked && (
+				<motion.div
+					className='flex flex-col gap-4 mt-4  px-4 pb-4'
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: animationDurationTime }}>
+					<motion.p>{description}</motion.p>
+					<motion.div className='flex items-center gap-2'>
+						<FontAwesomeIcon icon={faWindowMaximize} />
+						<motion.a className='text-linkedin-blue hover:underline' href={link} target='_blank'>
+							{link}
+						</motion.a>
+					</motion.div>
+				</motion.div>
+			)}
+		</motion.div>
+	);
 };
 
 export default GalacticRebornCard;
